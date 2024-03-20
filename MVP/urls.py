0aad0128082097f4cf37d0263e8c2dfe01b3ab10
Kami-don <bigoodmorning1990@gmail.com>
2024-03-20 07:52:22 -0400

    update urls and settings

--- MVP/urls.py
+++ MVP/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for APIserver project.
+URL configuration for MVP project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
